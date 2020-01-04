3f70458bbbfc667210f153dec69c208f49831f59
sray1 <sray12345678@gmail.com>
2020-01-03 18:11:13 -0800

    Add DrHlp4_0

---
+++ DrHlp4_0/CAFE/UnStres/~HMBak00/_vti_cnf/UNSTRES.isl
@@ -0,0 +1,6 @@
+vti_encoding:SR|utf8-nl
+vti_timelastmodified:TR|27 Mar 1997 15:48:54 -0000
+vti_extenderversion:SR|6.0.2.5516
+vti_cacheddtm:TX|27 Mar 1997 15:48:54 -0000
+vti_filesize:IR|0
+vti_backlinkinfo:VX|
---
+++ DrHlp4_0/GiG/Node/nodemap.h
@@ -0,0 +1,16 @@
+HelpNodeContents1	1001	; Node
+WhatisaNode.2	1002	; What is a Node
+WhydoweneedNode.3	1003	; Why do we need Node
+HowmanykindsofNode.4	1004	; How many kinds of Node
+InputNode.5	1005	; Input Node
+ControlNode.6	1006	; Control Node
+MeshNode.7	1007	; Mesh Node
+Howdowegenerate.8	1008	; How do we generate
+InputNodeMode.9	1009	; Input Node Mode
+On.the.flyMode.10	1010	; On-the-fly Mode
+GlossaryOfTerms.11	1011	; Glossary of Terms
+GlossKinematic.12	1012	; D2HGlossKinematic
+GlossMeshNode.13	1013	; D2HGlossMeshNode
+GlossControlNode.14	1014	; D2HGlossControlNode
+GlossInputNode.15	1015	; D2HGlossInputNode
+GlossNode.16	1016	; D2HGlossNode
